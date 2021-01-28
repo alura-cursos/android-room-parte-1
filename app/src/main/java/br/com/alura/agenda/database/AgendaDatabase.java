bd7fa259d5c4c8ccdccf6f44e17cd20a9b360df3
Alex Felipe <alexfelipevieira@gmail.com>
2021-01-28 14:13:08 -0300

    atualizando aula

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -1,12 +1,13 @@
 package br.com.alura.agenda.database;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
-import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
