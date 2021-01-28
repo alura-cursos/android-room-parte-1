04125755639943c898f884a71994764016a4a56a
Alex Felipe <alexfelipevieira@gmail.com>
2021-01-28 14:13:08 -0300

    atualizando projeto

--- app/src/main/java/br/com/alura/agenda/model/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -1,6 +1,6 @@
 package br.com.alura.agenda.model;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.io.Serializable;
 
--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,9 +2,9 @@ package br.com.alura.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
